dcad7a8a8b7b9061f6e409d39afeeb6c42cf7b43
basistakip <yapiisleribanu@gmail.com>
2025-03-23 01:48:53 +0300

    Update script.js

--- interaktif/yerleske/script.js
+++ interaktif/yerleske/script.js
@@ -1,1 +1,300 @@
+const hotspots = [
+  
+   {"name": "", "coords": [1619, 435], "href": "1"},
+    {"name": "DM (EY1)", "coords": [1518, 416, 1620, 416, 1620, 448.71, 1518, 448.71], "href": "1"},
+    {"name": "1-YÜK AYIRICI GİRİŞ ÇIKIŞ", "coords": [1518, 456, 1620, 456, 1620, 488.71, 1518, 488.71], "href": "1", "image": "https://i.hizliresim.com/74p9dkk.jpeg"},
+    {"name": "2-YURTA GİDEN", "coords": [1518, 488.71, 1620, 488.71, 1620, 521.43, 1518, 521.43], "href": "", "image": ""},
+    {"name": "3-AKIM GERİLİM ÖLÇÜM", "coords": [1518, 521.43, 1620, 521.43, 1620, 554.14, 1518, 554.14], "href": "2", "image": ""},
+    {"name": "4-İÇ İHTİYAÇ", "coords": [1518, 554.14, 1620, 554.14, 1620, 586.86, 1518, 586.86], "href": "", "image": ""},
+    {"name": "5-2500kVA(EY4 GiDN(MRKZİ DRSLK)Kesicili Giriş Çıkış", "coords": [1518, 586.86, 1620, 586.86, 1620, 619.57, 1518, 619.57], "href": "", "image": ""},
+    {"name": "6-1250KVA REKTÖRLÜK İBFF ÇIKIŞ", "coords": [1518, 619.57, 1620, 619.57, 1620, 652.29, 1518, 652.29], "href": "", "image": ""},
+    {"name": "7-2500KVA ÇIKIŞ YEDEK", "coords": [1518, 652.29, 1620, 652.29, 1620, 680, 1518, 680], "href": "", "image": ""},
+  
+  
+    {"name": "TM1(EY2)", "coords": [1740, 551, 1815, 551, 1815, 619.67, 1740, 619.67], "href": "1", "image": "https://i.hizliresim.com/74p9dkk.jpeg"},
+    {"name": "° 1 Yük Ayırıcı Giriş Çıkış", "coords": [1740, 591, 1815, 591, 1815, 659.67, 1740, 659.67], "href": "", "image": ""},
+    {"name": "° 2 Giriş Çıkış Ayırıcı Kesici", "coords": [1740, 659.67, 1815, 659.67, 1815, 728.34, 1740, 728.34], "href": "", "image": ""},
+    {"name": "° T-1 TRAFO 1250VA", "coords": [1740, 728.34, 1815, 728.34, 1815, 767, 1740, 767], "href": "", "image": ""},
+  
+  
+ 
+    {"name": "ADP-1(EY3)", "coords": [1740, 286, 1815, 286, 1815, 334.67, 1740, 334.67]
 
+
+
+
+
+
+
+
+
+
+
+
+
+, "href": "1"},
+    {"name": "°ADP+Jeneratör Trnsf", "coords": [1735, 340, 1815, 340, 1815, 421.5, 1735, 421.5], "href": "", "image": "https://i.hizliresim.com/74p9dkk.jpeg"},
+    {"name": "° K1 KOMPANZASYON", "coords": [1735, 421.5, 1815, 421.5, 1815, 503, 1735, 503], "href": "", "image": ""}
+,
+ 
+    {"name": "TM2+ADP2(EY4)", "coords": [1113, 165, 1299, 165, 1299, 185.25, 1113, 185.25]
+
+
+
+
+
+
+
+
+
+
+
+, "href": "1"},
+  
+  
+    {"name": "° ADP2+ JENERATÖR TRANSFER", "coords": [1085, 187, 1116.71, 187, 1116.71, 298, 1085, 298], "href": "https://drive.google.com/drive/folders/1ENDDvy4TniyCErR6AHEax8g8ZdsrnJaYgZLRNGxrwHH-jlh7RzaHgBnsTmioswmd93S6J7yw?usp=drive_link", "image": "https://i.hizliresim.com/74p9dkk.jpeg"},
+    {"name": "° K-2 KOMPANZASYON", "coords": [1116.71, 187, 1148.42, 187, 1148.42, 298, 1116.71, 298], "href": "https://drive.google.com/drive/folders/1ENDDvy4TniyCErR6AHEax8g8ZdsrnJaYgZLRNGxrwHH-jlh7RzaHgBnsTmioswmd93S6J7yw?usp=drive_link", "image": "https://i.hizliresim.com/74p9dkk.jpeg"},
+    {"name": "° 1-Yük Ayırıcılı Giriş/Çıkış 2500kVA", "coords": [1148.42, 187, 1180.13, 187, 1180.13, 298, 1148.42, 298], "href": "https://drive.google.com/drive/folders/1ENDDvy4TniyCErR6AHEax8g8ZdsrnJaYgZLRNGxrwHH-jlh7RzaHgBnsTmioswmd93S6J7yw?usp=drive_link", "image": "https://i.hizliresim.com/74p9dkk.jpeg"},
+    {"name": "° 2-Kesicili Giriş Çıkış ÖYM", "coords": [1180.13, 187, 1211.84, 187, 1211.84, 298, 1180.13, 298], "href": "https://drive.google.com/drive/folders/1ENDDvy4TniyCErR6AHEax8g8ZdsrnJaYgZLRNGxrwHH-jlh7RzaHgBnsTmioswmd93S6J7yw?usp=drive_link", "image": "https://i.hizliresim.com/74p9dkk.jpeg"},
+    {"name": "° 3-Kesicili Trafo Koruma Spor Akademi Giriş/Çıkış", "coords": [1211.84, 187, 1243.55, 187, 1243.55, 298, 1211.84, 298], "href": "https://drive.google.com/drive/folders/1ENDDvy4TniyCErR6AHEax8g8ZdsrnJaYgZLRNGxrwHH-jlh7RzaHgBnsTmioswmd93S6J7yw?usp=drive_link", "image": "https://i.hizliresim.com/74p9dkk.jpeg"},
+    {"name": "° 4-Çıkış_2500kva", "coords": [1243.55, 187, 1275.26, 187, 1275.26, 298, 1243.55, 298], "href": "https://drive.google.com/drive/folders/1ENDDvy4TniyCErR6AHEax8g8ZdsrnJaYgZLRNGxrwHH-jlh7RzaHgBnsTmioswmd93S6J7yw?usp=drive_link", "image": "https://i.hizliresim.com/74p9dkk.jpeg"},
+    {"name": "° 5)T-2 TRAFO 2500kVA", "coords": [1275.26, 187, 1307, 187, 1307, 298, 1275.26, 298], "href": "https://drive.google.com/drive/folders/1ENDDvy4TniyCErR6AHEax8g8ZdsrnJaYgZLRNGxrwHH-jlh7RzaHgBnsTmioswmd93S6J7yw?usp=drive_link", "image": "https://i.hizliresim.com/74p9dkk.jpeg"},
+    
+  
+    {
+        "name": "ADP-3(EY5)",
+        "coords": [1566, 216, 1659, 216, 1659, 236.25, 1566, 236.25],
+        "href": "1",
+        "image": "https://i.hizliresim.com/74p9dkk.jpeg"
+    },
+    {
+        "name": "K-3 KOMPANZASYON",
+        "coords": [1565, 238, 1659, 238, 1659, 293.65, 1565, 293.65],
+        "href": "",
+        "image": ""
+    },
+    {
+        "name": "ADP3+JENRTR TRANS",
+        "coords": [1565, 293.65, 1659, 293.65, 1659, 349.3, 1565, 349.3],
+        "href": "",
+        "image": ""
+    },
+  
+    {"name": "TM-3(EY6)", "coords": 
+[1566, 10, 1659, 10, 1659, 30.25, 1566, 30.25]
+
+
+
+, "href": "1", "image": "https://i.hizliresim.com/74p9dkk.jpeg"},
+    {"name": "1)Yük Ayırıcılı Giriş Çıkış", "coords": [1566, 35, 1659, 35, 1659, 75.25, 1566, 75.25], "href": "", "image": ""},
+    {"name": "2)Kesicili Giriş Çıkış", "coords": [1566, 75.25, 1659, 75.25, 1659, 115.5, 1566, 115.5], "href": "", "image": ""},
+    {"name": "3)Kesicili Trafo Koruma", "coords": [1566, 115.5, 1659, 115.5, 1659, 155.75, 1566, 155.75], "href": "", "image": ""},
+  {"name": "T3-TRAFO 1600kVA", "coords": [1566, 155.75, 1659, 155.75, 1659, 196, 1566, 196], "href": "", "image": ""},
+  
+  
+    {"name": "TM4(EY7)", "coords": [439, 355, 521, 355, 521, 375, 439, 375]
+, "href": "1"},
+    
+    {"name": "T4-TRAFO 800kVA", "coords": [439, 385, 521, 385, 521, 422, 439, 422], "href": "", "image": ""},
+    {"name": "3-Kesici Trafo koruma 800kVA", "coords": [439, 422, 521, 422, 521, 459, 439, 459], "href": "", "image": ""},
+    {"name": "2-Kesicili Spor Salonu Giriş Çıkış", "coords": [439, 459, 521, 459, 521, 496, 439, 496], "href": "", "image": ""},
+    {"name": "1-Yük Ayırıcı Giriş Çıkış", "coords": [439, 496, 521, 496, 521, 533, 439, 533], "href": "", "image": ""},
+  
+  
+       {"name": "TM-ADP4(EY8)", "coords": [439, 160, 521, 160, 521, 187, 439, 187]
+, "href": "1"},
+    {"name": "ADP4+JENERATÖR TRANS", "coords": [440, 186, 521, 186, 521, 262.5, 440, 262.5], "href": "", "image": ""},
+    {"name": "K4 KOMPANZASYON", "coords": [440, 262.5, 521, 262.5, 521, 339, 440, 339], "href": "", "image": ""},
+    
+
+
+   {"name": "TM5-ADP5(EY9)", "coords": [583.8, 505, 682.2, 505, 682.2, 528.1, 583.8, 528.1]
+
+, "href": "1"},
+    {"name": "° 1)Yük Ayırıcılı Giriş Hücresi", "coords": [579, 532, 618.67, 532, 618.67, 601, 579, 601], "href": "", "image": "https://i.hizliresim.com/74p9dkk.jpeg"},
+    {"name": "° 2)Trafo Koruma 1250kVA", "coords": [618.67, 532, 658.34, 532, 658.34, 601, 618.67, 601], "href": "", "image": "https://i.hizliresim.com/74p9dkk.jpeg"},
+    {"name": "° T5-TRAFO 1250kVA", "coords": [658.34, 532, 698, 532, 698, 601, 658.34, 601], "href": "", "image": "https://i.hizliresim.com/74p9dkk.jpeg"}
+];
+
+function createHotspots() {
+    const image = document.getElementById('krokiImage');
+    const svg = document.getElementById('mapSvg');
+    const tooltipContainer = document.getElementById('tooltip-container');
+    const tooltipImage = document.getElementById('tooltip-image');
+    svg.innerHTML = '';
+
+    const originalWidth = image.naturalWidth;
+    const originalHeight = image.naturalHeight;
+    const currentWidth = image.clientWidth;
+    const currentHeight = image.clientHeight;
+
+    hotspots.forEach(hotspot => {
+        const scaledCoords = hotspot.coords.map((coord, index) => {
+            return index % 2 === 0
+                ? (coord / originalWidth) * currentWidth
+                : (coord / originalHeight) * currentHeight;
+        });
+
+        const polygon = document.createElementNS("http://www.w3.org/2000/svg", "polygon");
+        polygon.setAttribute("points", scaledCoords.join(" "));
+        polygon.setAttribute("style", "pointer-events: auto;");
+        polygon.addEventListener('click', () => {
+            window.location.href = hotspot.href;
+        });
+
+        polygon.addEventListener('mouseover', () => {
+            const textElement = document.querySelector(`text[data-name="${hotspot.name}"]`);
+            if (textElement) {
+                textElement.classList.add('highlight');
+            }
+            if (hotspot.image) {
+                tooltipImage.src = hotspot.image;
+                tooltipContainer.style.display = 'block';
+            }
+        });
+
+        polygon.addEventListener('mouseout', () => {
+            const textElement = document.querySelector(`text[data-name="${hotspot.name}"]`);
+            if (textElement) {
+                textElement.classList.remove('highlight');
+            }
+            tooltipContainer.style.display = 'none';
+        });
+        svg.appendChild(polygon);
+    });
+
+    hotspots.forEach(hotspot => {
+        const scaledCoords = hotspot.coords.map((coord, index) => {
+            return index % 2 === 0
+                ? (coord / originalWidth) * currentWidth
+                : (coord / originalHeight) * currentHeight;
+        });
+
+        const text = document.createElementNS("http://www.w3.org/2000/svg", "text");
+        const textX = Math.max(...scaledCoords.filter((_, i) => i % 2 === 0)) - 1;
+        const textY = Math.min(...scaledCoords.filter((_, i) => i % 2 !== 0)) + 10;
+
+        text.setAttribute("x", textX);
+        text.setAttribute("y", textY);
+
+        if (!hotspot.name.startsWith("°")) {
+            text.setAttribute("transform", `rotate(0, ${textX}, ${textY})`);
+        } else {
+            text.setAttribute("transform", `rotate(-35, ${textX}, ${textY})`);
+        }
+
+        text.setAttribute("text-anchor", "end");
+        text.setAttribute("data-name", hotspot.name);
+        text.textContent = hotspot.name;
+        text.setAttribute("style", "pointer-events: none; z-index: 9999;");
+
+        svg.appendChild(text);
+    });
+}
+
+function createPath(pathCoords, color, delay) {
+    const image = document.getElementById('krokiImage');
+    const svg = document.getElementById('mapSvg');
+
+    const originalWidth = image.naturalWidth;
+    const originalHeight = image.naturalHeight;
+    const currentWidth = image.clientWidth;
+    const currentHeight = image.clientHeight;
+
+    let pathData = "";
+    pathCoords.forEach((coord, i) => {
+        const x = (coord[0] / originalWidth) * currentWidth;
+        const y = (coord[1] / originalHeight) * currentHeight;
+        if (i === 0) {
+            pathData += `M ${x},${y} `;
+        } else {
+            pathData += `L ${x},${y} `;
+        }
+    });
+
+    const path = document.createElementNS("http://www.w3.org/2000/svg", "path");
+    path.setAttribute("d", pathData);
+    path.setAttribute("fill", "none");
+    path.setAttribute("stroke", color);
+    path.setAttribute("stroke-width", "4");
+    svg.insertBefore(path, svg.firstChild);
+
+    const circle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+    circle.setAttribute("r", "5");
+    circle.setAttribute("fill", color);
+    circle.setAttribute("fill-opacity", "0.8");
+    circle.setAttribute("stroke", "#ffffff");
+    circle.setAttribute("stroke-width", "1");
+    circle.setAttribute("filter", "url(#glow)");
+    svg.insertBefore(circle, svg.firstChild);
+
+    const animateMotion = document.createElementNS("http://www.w3.org/2000/svg", "animateMotion");
+    animateMotion.setAttribute("dur", "8s");
+    animateMotion.setAttribute("repeatCount", "indefinite");
+    animateMotion.setAttribute("path", pathData);
+    animateMotion.setAttribute("begin", `${delay}s`);
+    circle.appendChild(animateMotion);
+
+    const animateSize = document.createElementNS("http://www.w3.org/2000/svg", "animate");
+    animateSize.setAttribute("attributeName", "r");
+    animateSize.setAttribute("values", "5;8;5");
+    animateSize.setAttribute("dur", "0.5s");
+    animateSize.setAttribute("repeatCount", "indefinite");
+    circle.appendChild(animateSize);
+}
+// renk turkuaz EY1 giriş
+const pathCoords1 = [[1600, 1000],
+    [1500, 700], [1500, 468], [1540, 468], [1540, 668]
+];
+// renk bordo ey2 ey3 giriş
+const pathCoords2 = [
+    [1540, 638], [1770, 607], [1770, 760], [1850, 760] ,[1850, 400] ,[1790, 400] ,[1790, 480],[1790, 310], [1700, 450]
+];
+// renk sarı ey4 giriş
+const pathCoords3 = [
+    [1540, 608], [1460, 608], [1460, 390], [1335, 170]  , [1168, 170] , [1168, 200] , [1300, 200]
+];
+// renk green ey7 ve ey8 giriş
+const pathCoords4 = [
+    [1227, 200], [1227, 315], [1120, 325], [1050, 370], [720, 410], [670, 330], [530, 345], [530, 515], [490, 515], [490, 230], [580, 230], [580, 430]
+];
+
+//renk ping ey6 ve ey5 giriş
+const pathCoords5 = [[1190, 200],[1190, 310],[1350, 310],[1350, 170],[1550, 48],[1630, 48],[1630, 315],[1750, 240]];
+
+// renk green ey 9 giriş
+const pathCoords6 = [[490, 480],[550, 480],[550, 580],[690, 580],[600, 680]];
+// renk green ey 5 den çıkış
+const pathCoords7 = [[1630, 315],[1520, 190]];
+// renk merkezi dersliğe giriş
+const pathCoords8 = [[1290, 200],[1290, 150],[1100, 150],[1100, 310],[1160, 350]];
+
+
+window.addEventListener('load', () => {
+    createHotspots();
+    createPath(pathCoords1, "#00ffff", 0);
+    createPath(pathCoords2, "#ff00ff", 2);
+    createPath(pathCoords3, "#ffff00", 3);
+ createPath(pathCoords4, "#97bf22", 
+4); 
+  createPath(pathCoords5, "#ab099c", 5); 
+ createPath(pathCoords6, "#09ab9f", 6); 
+ createPath(pathCoords7, "#ab099c", 7);
+  createPath(pathCoords8, "#0981ab", 8);
+  
+});
+
+window.addEventListener('resize', () => {
+    const svg = document.getElementById('mapSvg');
+    svg.innerHTML = '';
+
+    createHotspots();
+    createPath(pathCoords1, "#00ffff", 0);
+    createPath(pathCoords2, "#ff00ff", 2);
+    createPath(pathCoords3, "#ffff00", 3);
+   createPath(pathCoords4, "#97bf22", 4);
+  createPath(pathCoords5, "#ab099c",5);
+createPath(pathCoords6, "#09ab9f",6);
+ createPath(pathCoords7, "#ab099c", 7);
+   createPath(pathCoords8, "#0981ab", 8);
+});
