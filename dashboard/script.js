86611f83e973f456ecb98a46cc5797d54d2be911
basistakip <yapiisleribanu@gmail.com>
2025-04-02 02:02:51 +0300

    Create script.js

---
+++ dashboard/script.js
@@ -0,0 +1,11 @@
+/*
+ * Watch this tutorial on YouTube
+ * https://youtu.be/QlZkfmofzHU
+ */
+
+const resizeBtn = document.getElementById("resize");
+
+resizeBtn.addEventListener("click", function (e) {
+  e.preventDefault();
+  document.body.classList.toggle("sb-expand");
+});
